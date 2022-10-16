f988357550cbd9483fca5615420ee81c4aeed48d
riad nbou <riadnbou@192.168.1.2>
2022-10-16 15:00:15 +0100

    need comments and readme

--- face_mixer.py
+++ face_mixer.py
@@ -1,6 +1,9 @@
+from ast import arg
 import cv2
 import mediapipe as mp
 import numpy as np
+import argparse
+
 
 class face_extractor():
     def __init__(self,image):
@@ -56,15 +59,17 @@ class face_extractor():
 
         p1 = round(r_eye_dis/l_eye_dis,2)
         p2 = round(third_eye_dis/mouth_dis,1)
-
-        if p1>= 0.9 and p1 <= 1.1 and p2 <= 1.4 and p2 >= 1:
+        if p1>= 0.8 and p1 <= 1.2 and p2 <= 1.6 and p2 >= 0.9:
             return True
         return False
 
 class face_mixer():
     def __init__(self,image1,image2):
         self.image1 = image1
+        self.backup1 = image1.copy()
         self.image2 = image2
+        self.backup2 = image2.copy()
+        self.output = None
     def background_blure(self):
         mp_selfie_segmentation = mp.solutions.selfie_segmentation
         with mp_selfie_segmentation.SelfieSegmentation(model_selection=0) as selfie_segmentation:
@@ -74,7 +79,7 @@ class face_mixer():
             blured_img = cv2.GaussianBlur(self.image1,(25,25),200)
             rev_mask = 1-mask
             self.image1 = (self.image1 * mask + blured_img * rev_mask).astype(np.uint8)
-    def extract_face(self):
+    def extract_face_and_merge(self):
         mp_drawing = mp.solutions.drawing_utils
         mp_drawing_styles = mp.solutions.drawing_styles
         mp_face_mesh = mp.solutions.face_mesh
@@ -99,7 +104,8 @@ class face_mixer():
             mk = np.zeros((h+2, w+2), np.uint8)
             cv2.floodFill(im_th,mk,(0,0),(255,255,255))
             im_th = (255 - im_th)
-            mask = cv2.GaussianBlur(im_th,(25,25),200) / 255
+            mask = cv2.GaussianBlur(im_th,(55,55),200) / 255
+            # mask = im_th / 255
             self.image2 = (mask * self.image2).astype(np.uint8)
 
 
@@ -117,35 +123,39 @@ class face_mixer():
             h0,w0,_ = im_th0.shape
             mk0 =  np.zeros((h0+2, w0+2), np.uint8)
             cv2.floodFill(im_th0,mk0,(0,0),(255,255,255))
-            mask0 = cv2.GaussianBlur(im_th0,(25,25),200) / 255
+            mask0 = cv2.GaussianBlur(im_th0,(55,55),200) / 255
+            # mask0 = im_th0 / 255
             self.image1 = (mask0 * self.image1).astype(np.uint8)
 
             lm = np.array([[l.x * w, l.y * h] for l in face_landmarks.landmark],np.float32).reshape(-1,1,2)
             lm0 = np.array([[l.x * w0, l.y * h0] for l in face_landmarks0.landmark],np.float32).reshape(-1,1,2)
 
-            M, m = cv2.findHomography(lm, lm0, cv2.RANSAC,5.0)
+            M, m = cv2.findHomography(lm, lm0, 0,confidence = 1)
             im_dst = cv2.warpPerspective(self.image2, M,(w0,h0))
+            self.backup2 = cv2.warpPerspective(self.backup2, M,(w0,h0))
+            overlaping_mask = (im_dst.astype(np.uint64) + self.image1.astype(np.uint64) )>255
+            overlaping_mask = np.array([[[j.any()]*3 for j in i] for i in overlaping_mask])
+            im_dst = im_dst + self.image1
+            self.output = np.where(overlaping_mask, self.backup2, im_dst)
 
-            im_dst = ((1 - mask0) * im_dst).astype(np.uint8) + self.image1
-            cv2.imshow("1",self.image2)
-            
-            cv2.imshow("2",im_dst)
-            cv2.imshow("3",self.image1)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-    
-            
-    
+
   
 
 
 
 if __name__ == "__main__":
-    image1 = cv2.imread("4.jpeg")
-    image2 = cv2.imread("test2.jpeg")
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-m", help="the main face image",required=True)
+    parser.add_argument("-s", help="the secondary face image",required=True)
+    parser.add_argument("-o", help="output image",default="output.jpeg")
+    args = parser.parse_args()
+
+    image1 = cv2.imread(args.m)
+    image2 = cv2.imread(args.s)
     face_extract1 = face_extractor(image1)
     face_extract2 = face_extractor(image2)
-    print("\n\n\n\n")
     face_extract1.detect_faces()
     if face_extract1.number_of_faces == 0:
         print("no face was detected in the image1")
@@ -168,6 +178,12 @@ if __name__ == "__main__":
         print("face should be facing the camera image2")
         exit(0)
     
+
+
     face_m = face_mixer(image1,image2)
     face_m.background_blure()
-    face_m.extract_face()
+    face_m.extract_face_and_merge()
+    cv2.imwrite(args.o, face_m.output)
+    cv2.imshow("output",face_m.output)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
